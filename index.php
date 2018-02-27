d2755132994b05e05b15243e490c26dd89ffe88b
apblood <36895495+apblood@users.noreply.github.com>
2018-02-28 00:28:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $access_token = 'EAAb8FMoYCnABAK94BCBrZCCOFhMkeA80ZAEAWBKql5IqRjiKHiqZB5NCasiDkSCsSWBrzZCplv3ykktlc0zKIcAGTrU27G3lGb0albCSJZCCEU3hyMScMm67ScYzQ6GedH6ISyT93LIaSZAo7uLZCNL2kvVpZCvEENqf2ntUH2a6JAZDZD';
 
 if (isset($_GET['hub_verify_token'])) {
-    if ($_GET[''hub_verify_token']===$access_token) {
+    if ($_GET['hub_verify_token']===$access_token) {
         echo $_GET['hub_challenge'];
         return;
     }
